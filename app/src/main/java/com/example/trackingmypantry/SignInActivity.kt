a9ee14fa96ba3b964b63dfafd56b3e24d0f44f43
bogo8liuk <lucaborghi99@gmail.com>
2021-05-13 21:58:48 +0200

    setEditText private method

--- app/src/main/java/com/example/trackingmypantry/SignInActivity.kt
+++ app/src/main/java/com/example/trackingmypantry/SignInActivity.kt
@@ -1,11 +1,21 @@
 package com.example.trackingmypantry
 
 import android.os.Bundle
+import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 
 class SignInActivity : AppCompatActivity() {
+    private fun setEditText(editText: EditText, hint: Int, inputType: Int) {
+        editText.layoutParams = ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT)
+        editText.setEms(10)
+        editText.setHint(hint)
+        editText.setRawInputType(inputType)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign) //TODO: change layout
--- app/src/main/java/com/example/trackingmypantry/SignUpActivity.kt
+++ app/src/main/java/com/example/trackingmypantry/SignUpActivity.kt
@@ -2,11 +2,21 @@ package com.example.trackingmypantry
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 
 class SignUpActivity : AppCompatActivity() {
+    private fun setEditText(editText: EditText, hint: Int, inputType: Int) {
+        editText.layoutParams = ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT)
+        editText.setEms(10)
+        editText.setHint(hint)
+        editText.setRawInputType(inputType)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
         setContentView(R.layout.activity_sign)
