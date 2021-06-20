62f6a75fff75826e24851db0faaec14f90a39e2d
bogo8liuk <lucaborghi99@gmail.com>
2021-06-20 11:47:57 +0200

    product dao

--- app/src/main/java/com/example/trackingmypantry/db/daos/ProductDao.kt
+++ app/src/main/java/com/example/trackingmypantry/db/daos/ProductDao.kt
@@ -1,8 +1,27 @@
 package com.example.trackingmypantry.db.daos
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.trackingmypantry.db.entities.Product
 
 @Dao
 interface ProductDao {
+    @Query("select * from Product")
+    fun getAllProducts(): List<Product>
 
+    @Query("select * from Product where barcode like :barcode")
+    fun getProductByBarcode(barcode: String): Product
+
+    @Query("select * from Product where name like :name")
+    fun getProductsByName(name: String): List<Product>
+
+    @Insert
+    fun insertProduct(product: Product)
+
+    @Query("update product set image = :image where barcode like :barcode")
+    fun insertImageForProduct(barcode: String, image: String)
+
+    @Query("update product set personal_comment = :comment where barcode like :barcode")
+    fun insertCommentForProduct(barcode: String, comment: String)
 }
