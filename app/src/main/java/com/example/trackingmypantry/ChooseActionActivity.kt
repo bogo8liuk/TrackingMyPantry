c3e5326ed0ea2e9ac7da915d6294d95728cf5594
bogo8liuk <lucaborghi99@gmail.com>
2021-06-27 17:21:51 +0200

    wip: buy handler needs a ViewModel

--- app/src/main/java/com/example/trackingmypantry/ChooseActionActivity.kt
+++ app/src/main/java/com/example/trackingmypantry/ChooseActionActivity.kt
@@ -10,9 +10,8 @@ import androidx.appcompat.widget.AppCompatButton
 import com.example.trackingmypantry.lib.BuyHandler
 
 class ChooseActionActivity: AppCompatActivity() {
-    private val MIN_BUYABLE_ITEMS_NO = 1  // minimum number of objects that can be bought at a time
-    private val MAX_BUYABLE_ITEMS_NO = 20 // maximum number of objects that can be bought at a time
-
+    private val DEFAULT_BARCODE = "00000000"
+    private lateinit var barcode: String
     private lateinit var buyButton: AppCompatButton
     private lateinit var localButton: AppCompatButton
     private lateinit var shareButton: AppCompatButton
@@ -21,21 +20,18 @@ class ChooseActionActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         this.setContentView(R.layout.activity_choose)
+        barcode = this.intent.extras?.getString("barcode", DEFAULT_BARCODE) ?: DEFAULT_BARCODE
         buyButton = this.findViewById(R.id.buyButton)
         localButton = this.findViewById(R.id.localButton2)
         shareButton = this.findViewById(R.id.shareButton)
 
         buyButton.setOnClickListener {
-            var numberPicker = NumberPicker(this)
-            numberPicker.minValue = MIN_BUYABLE_ITEMS_NO;
-            numberPicker.maxValue = MAX_BUYABLE_ITEMS_NO;
             AlertDialog.Builder(this)
                 .setTitle(R.string.buy1)
-                .setMessage("Choose the number of items you want to buy")
-                .setView(numberPicker)
-                .setNegativeButton(R.string.cancel, null)
-                .setPositiveButton(R.string.buy1, DialogInterface.OnClickListener { dialog, which ->
-                    BuyHandler.buy(numberPicker.value)
+                .setMessage("Are you sure of the purchase?")
+                .setNegativeButton(R.string.negative, null)
+                .setPositiveButton(R.string.positive, DialogInterface.OnClickListener { dialog, which ->
+                    BuyHandler.buy(this, "00000000" /*TODO: get the barcode from the right view */)
                 })
                 .show()
         }
--- app/src/main/java/com/example/trackingmypantry/lib/BuyHandler.kt
+++ app/src/main/java/com/example/trackingmypantry/lib/BuyHandler.kt
@@ -1,13 +1,25 @@
 package com.example.trackingmypantry.lib
 
+import android.content.Context
+
 class BuyHandler() {
     companion object {
         private fun serviceNotify() {
 
         }
 
-        fun buy(barcode: String, quantity: Int) {
+        fun buy(context: Context, barcode: String) {
+            HttpHandler.serviceGetProduct(
+                context,
+                barcode,
+                "00000000", // TODO: get the right access token
+                { res ->
+                    // TODO: implement a ViewModel
+                },
+                { status_code, _ ->
 
+                }
+            )
         }
     }
 }
