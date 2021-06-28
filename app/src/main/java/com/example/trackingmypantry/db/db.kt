86ef235a5bc51f196c0fe89e8a23b9b48e248094
bogo8liuk <lucaborghi99@gmail.com>
2021-06-28 17:10:54 +0200

    wip: ViewModel

--- app/src/main/java/com/example/trackingmypantry/db/db.kt
+++ app/src/main/java/com/example/trackingmypantry/db/db.kt
@@ -1,6 +1,14 @@
 package com.example.trackingmypantry.db
 
+import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.trackingmypantry.db.daos.ItemDao
+import com.example.trackingmypantry.db.entities.Item
+import com.example.trackingmypantry.db.entities.Place
+import com.example.trackingmypantry.db.entities.PlaceSuggestion
+import com.example.trackingmypantry.db.entities.Suggestion
 
+@Database(entities = [Item::class, Place::class, Suggestion::class, PlaceSuggestion::class], version = 1)
 abstract class db: RoomDatabase() {
+    abstract fun itemDao(): ItemDao
 }
