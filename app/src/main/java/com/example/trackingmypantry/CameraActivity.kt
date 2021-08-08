75a20a52cdea85f1f08e7e707857388c650b3b7a
bogo8liuk <lucaborghi99@gmail.com>
2021-08-08 16:33:21 +0200

    wip: Camera activity class

--- app/src/main/java/com/example/trackingmypantry/CameraActivity.kt
+++ app/src/main/java/com/example/trackingmypantry/CameraActivity.kt
@@ -1,11 +1,16 @@
 package com.example.trackingmypantry
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.AppCompatButton
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class CameraActivity(private val imageCapturing: Boolean) : AppCompatActivity() {
     protected lateinit var takePhotoButton: AppCompatButton
+    protected lateinit var cameraExecutor: ExecutorService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,5 +21,19 @@ class CameraActivity(private val imageCapturing: Boolean) : AppCompatActivity() 
         if (!imageCapturing) {
             this.takePhotoButton.visibility = android.view.View.GONE
         }
+        this.takePhotoButton.setOnClickListener {
+            //TODO: call `takePhoto()`
+        }
+
+        this.cameraExecutor = Executors.newSingleThreadExecutor()
+    }
+
+    protected fun cameraPermissionGranted(): Boolean {
+        return this.checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        this.cameraExecutor.shutdown()
     }
 }
