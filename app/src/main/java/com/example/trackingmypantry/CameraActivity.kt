97833a976c3c80e2f2d6e6b53c7a52c7e736e941
bogo8liuk <lucaborghi99@gmail.com>
2021-08-08 16:05:51 +0200

    camera activity super class

--- app/src/main/java/com/example/trackingmypantry/CameraActivity.kt
+++ app/src/main/java/com/example/trackingmypantry/CameraActivity.kt
@@ -2,10 +2,19 @@ package com.example.trackingmypantry
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.AppCompatButton
 
-class CameraActivity : AppCompatActivity() {
+class CameraActivity(private val imageCapturing: Boolean) : AppCompatActivity() {
+    protected lateinit var takePhotoButton: AppCompatButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_camera)
+        this.setContentView(R.layout.activity_camera)
+
+        this.takePhotoButton = this.findViewById(R.id.takePhotoButton)
+
+        if (!imageCapturing) {
+            this.takePhotoButton.visibility = android.view.View.GONE
+        }
     }
 }
