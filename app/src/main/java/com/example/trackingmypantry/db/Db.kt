32cd864714989ad9bc065f20b3972d0854f9c16b
bogo8liuk <lucaborghi99@gmail.com>
2021-08-13 16:33:46 +0200

    bug fix at compile time (bad import)

--- app/src/main/java/com/example/trackingmypantry/db/Db.kt
+++ app/src/main/java/com/example/trackingmypantry/db/Db.kt
@@ -4,12 +4,13 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.example.trackingmypantry.db.daos.ItemDao
+import com.example.trackingmypantry.db.entities.Collection
 import com.example.trackingmypantry.db.entities.Item
 import com.example.trackingmypantry.db.entities.Place
 import com.example.trackingmypantry.db.entities.PlaceSuggestion
 import com.example.trackingmypantry.db.entities.Suggestion
 
-@Database(entities = [Item::class, Place::class, Suggestion::class, PlaceSuggestion::class], version = 1)
+@Database(entities = [Collection::class, Item::class, Place::class, Suggestion::class, PlaceSuggestion::class], version = 1)
 @TypeConverters(Converter::class)
 abstract class Db: RoomDatabase() {
     abstract fun itemDao(): ItemDao
