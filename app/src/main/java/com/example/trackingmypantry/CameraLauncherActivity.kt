29c98c2560903041dedf31f7bd698e99705d8fda
bogo8liuk <lucaborghi99@gmail.com>
2021-08-22 15:31:30 +0200

    bug fix in camera launcher activity

--- app/src/main/java/com/example/trackingmypantry/CameraLauncherActivity.kt
+++ app/src/main/java/com/example/trackingmypantry/CameraLauncherActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.provider.MediaStore
 import androidx.activity.result.contract.ActivityResultContracts
 
@@ -13,26 +14,29 @@ open class CameraLauncherActivity : AppCompatActivity() {
     protected var encodedImage: Bitmap? = null
         private set
 
-    private val cameraLauncher = { success: (Bitmap) -> Unit, error: () -> Unit ->
-        this.registerForActivityResult(
+    private lateinit var successCallback: (Bitmap) -> Unit
+    private lateinit var errorCallback: () -> Unit
+
+    private val cameraLauncher = this.registerForActivityResult(
             ActivityResultContracts.StartActivityForResult()
         ) { result ->
             when (result.resultCode) {
                 RESULT_OK -> {
                     val bitmap = result.data!!.extras!!["data"] as Bitmap
                     this.encodedImage = bitmap
-                    success(bitmap)
+                    this.successCallback(bitmap)
                 }
 
                 else -> {
-                    error()
+                    this.errorCallback()
                 }
             }
         }
-    }
 
     protected fun cameraLaunch(success: (Bitmap) -> Unit, error: () -> Unit) {
         val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        this.cameraLauncher(success, error).launch(intent)
+        this.successCallback = success
+        this.errorCallback = error
+        this.cameraLauncher.launch(intent)
     }
 }
