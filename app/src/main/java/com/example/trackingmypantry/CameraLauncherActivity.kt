0b19085c54c9daba22d21a47ac5f17ca11d41d00
bogo8liuk <lucaborghi99@gmail.com>
2021-08-22 11:39:49 +0200

    CameraLauncherActivity

---
+++ app/src/main/java/com/example/trackingmypantry/CameraLauncherActivity.kt
@@ -0,0 +1,37 @@
+package com.example.trackingmypantry
+
+import android.content.Intent
+import android.graphics.Bitmap
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import androidx.activity.result.contract.ActivityResultContracts
+
+class CameraLauncherActivity : AppCompatActivity() {
+    val IMAGE_CAPTURE_REQUEST = 1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    private val cameraLauncher = { success: (Bitmap) -> Unit, error: () -> Unit ->
+        this.registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { result ->
+            when (result.resultCode) {
+                RESULT_OK -> {
+                    success(result.data!!.extras!!["data"] as Bitmap)
+                }
+
+                else -> {
+                    error()
+                }
+            }
+        }
+    }
+
+    protected fun launch(success: (Bitmap) -> Unit, error: () -> Unit) {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        this.cameraLauncher(success, error).launch(intent)
+    }
+}
