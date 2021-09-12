2dd1faf188483363e2b16912e50a31fd08861cd3
bogo8liuk <lucaborghi99@gmail.com>
2021-09-12 17:40:34 +0200

    CollectionsViewModel factory

--- app/src/main/java/com/example/trackingmypantry/CollectionsActivity.kt
+++ app/src/main/java/com/example/trackingmypantry/CollectionsActivity.kt
@@ -3,6 +3,11 @@ package com.example.trackingmypantry
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.GridView
+import androidx.activity.viewModels
+import com.example.trackingmypantry.db.entities.Collection
+import com.example.trackingmypantry.lib.adapters.CollectionsAdapter
+import com.example.trackingmypantry.lib.viewmodels.CollectionsViewModel
+import com.example.trackingmypantry.lib.viewmodels.CollectionsViewModelFactory
 
 class CollectionsActivity : AppCompatActivity() {
     private lateinit var gridView: GridView
@@ -12,5 +17,10 @@ class CollectionsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_collections)
 
         this.gridView = findViewById(R.id.collectionsGridView)
+        this.gridView.adapter = CollectionsAdapter(this, arrayOf<Collection>())
+
+        val model: CollectionsViewModel by viewModels {
+            CollectionsViewModelFactory(this.application)
+        }
     }
 }
