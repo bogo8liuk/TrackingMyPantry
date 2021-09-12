70021068f246b0cd006df77eda960cc6eb2e90e7
bogo8liuk <lucaborghi99@gmail.com>
2021-09-12 17:32:57 +0200

    collection adapter

--- app/src/main/java/com/example/trackingmypantry/lib/adapters/CollectionsAdapter.kt
+++ app/src/main/java/com/example/trackingmypantry/lib/adapters/CollectionsAdapter.kt
@@ -1,11 +1,45 @@
 package com.example.trackingmypantry.lib.adapters
 
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.ImageButton
+import android.widget.TextView
+import com.example.trackingmypantry.R
 import com.example.trackingmypantry.db.entities.Collection
 
-class CollectionsAdapter(private val collections: Array<Collection>): BaseAdapter() {
+class CollectionsAdapter(private val context: Context, private val collections: Array<Collection>): BaseAdapter() {
+    private val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+    private val MAX_COLOR = 6
+
+    private fun getColor(position: Int): Int {
+        when (position % MAX_COLOR) {
+            0 -> return R.color.red
+            1 -> return R.color.blue
+            2 -> return R.color.green
+            3 -> return R.color.orange
+            4 -> return R.color.purple_500
+            5 -> return R.color.lightblue
+        }
+
+        Log.e("Unreachable state", "There are just 6 possible colors for collections " +
+                "UI and black is not among them")
+        return R.color.black
+    }
+
+    private fun initView(view: View, position: Int): View {
+        val collectionButton = view.findViewById<ImageButton>(R.id.collectionButton)
+        val collectionText = view.findViewById<TextView>(R.id.collectionText)
+
+        collectionButton.setBackgroundResource(this.getColor(position))
+        collectionText.text = this.collections[position].name
+
+        return view
+    }
+
     override fun getCount(): Int {
         return this.collections.size
     }
@@ -19,6 +53,6 @@ class CollectionsAdapter(private val collections: Array<Collection>): BaseAdapte
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        TODO("Not yet implemented")
+        return convertView ?: this.initView(this.inflater.inflate(R.layout.collection_row, parent), position)
     }
 }
