b93d35c60a0a2c761ac378ffcc039f4922ae16e6
bogo8liuk <lucaborghi99@gmail.com>
2021-09-16 11:54:38 +0200

    bluetooth discovery

--- app/src/main/java/com/example/trackingmypantry/lib/connectivity/bluetooth/BlueHandler.kt
+++ app/src/main/java/com/example/trackingmypantry/lib/connectivity/bluetooth/BlueHandler.kt
@@ -4,13 +4,15 @@ import android.app.Activity
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
+import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 
 class BlueHandler {
     companion object {
-        const val DEFAULT_REQUEST_CODE = 0
+        const val BLUETOOTH_ENABLING_REQUEST_CODE = 0
+        const val DISCOVERABLE_REQUEST_CODE = 1
 
         /**
          * It returns true if classic bluetooth is supported, false otherwise
@@ -27,14 +29,14 @@ class BlueHandler {
         }
 
         /**
-         * It carries out a request of bluetooth connection if bluetooth is not enabled.
+         * It carries out a request of bluetooth enabling if bluetooth is not enabled.
          * @warning It calls `startActivityForResult`, so the request needs to be handled
          * by implementing `onActivityResult`.
          */
-        fun requestIfDisabled(activity: Activity, adapter: BluetoothAdapter) {
+        fun enableRequestIfDisabled(activity: Activity, adapter: BluetoothAdapter) {
             if (!adapter.isEnabled) {
                 val intent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-                activity.startActivityForResult(intent, DEFAULT_REQUEST_CODE)
+                activity.startActivityForResult(intent, BLUETOOTH_ENABLING_REQUEST_CODE)
             }
         }
 
@@ -57,6 +59,41 @@ class BlueHandler {
 
                     if (!continueAfterMatch) {
                         return
+                    }
+                }
+            }
+        }
+
+        /**
+         * It carries out a request to make the current device discoverable by other
+         * bluetooth devices for 5 minutes.
+         * @warning It calls `startActivityForResult`, so the request needs to be handled
+         * by implementing `onActivityResult`.
+         */
+        fun makeDiscoverable(activity: Activity) {
+            val intent = Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE).apply {
+                this.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300)
+            }
+            activity.startActivityForResult(intent, DISCOVERABLE_REQUEST_CODE)
+        }
+
+        /**
+         * Functions that wraps `startDiscovery()` of `BluetoothAdpater`.
+         */
+        fun startDiscovery(adapter: BluetoothAdapter): Boolean {
+            return adapter.startDiscovery()
+        }
+
+        /**
+         * It returns a `BroadcastReceiver` object that handles the case `stratDiscovery()`
+         * finds a device.
+         * @warning The receiver must unregistered.
+         */
+        fun bluetoothDeviceReceiver(handler: (BluetoothDevice?) -> Unit) = object: BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) {
+                when (intent.action) {
+                    BluetoothDevice.ACTION_FOUND -> {
+                        handler(intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE))
                     }
                 }
             }
