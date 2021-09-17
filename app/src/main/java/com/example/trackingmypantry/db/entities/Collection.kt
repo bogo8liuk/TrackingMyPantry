160f46142a695f0e5255e1261c3a4068165162b0
bogo8liuk <lucaborghi99@gmail.com>
2021-09-17 19:23:27 +0200

    name field made unique

--- app/src/main/java/com/example/trackingmypantry/db/entities/Collection.kt
+++ app/src/main/java/com/example/trackingmypantry/db/entities/Collection.kt
@@ -1,10 +1,14 @@
 package com.example.trackingmypantry.db.entities
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(
+    indices = arrayOf(Index(value = ["name"], unique = true))
+)
 data class Collection(
     @PrimaryKey(autoGenerate = true) val id: Long,
-    val name: String
+    @ColumnInfo(name = "name")val name: String  // Unique
 )
