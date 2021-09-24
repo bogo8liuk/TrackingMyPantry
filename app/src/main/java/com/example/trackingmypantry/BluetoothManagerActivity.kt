87f357bf893a31736f7c37c222cb49e0c3f6afd1
bogo8liuk <lucaborghi99@gmail.com>
2021-09-24 12:37:38 +0200

    BluetoothManagerActivity

---
+++ app/src/main/java/com/example/trackingmypantry/BluetoothManagerActivity.kt
@@ -0,0 +1,11 @@
+package com.example.trackingmypantry
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BluetoothManagerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        this.setContentView(R.layout.activity_bluetooth_manager)
+    }
+}
