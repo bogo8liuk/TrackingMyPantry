ad58f12d43cbad94e6f644d2dcecafbcc92c81f6
bogo8liuk <lucaborghi99@gmail.com>
2021-09-24 19:01:28 +0200

    bluetooth manager activity layout

--- app/src/main/java/com/example/trackingmypantry/BluetoothManagerActivity.kt
+++ app/src/main/java/com/example/trackingmypantry/BluetoothManagerActivity.kt
@@ -2,10 +2,26 @@ package com.example.trackingmypantry
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.appcompat.widget.AppCompatButton
+import androidx.recyclerview.widget.RecyclerView
 
 class BluetoothManagerActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var devicesDescText: TextView
+    private lateinit var pairButton: AppCompatButton
+    private lateinit var acceptButton: AppCompatButton
+    private lateinit var progressBar: ProgressBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         this.setContentView(R.layout.activity_bluetooth_manager)
+
+        this.recyclerView = this.findViewById(R.id.btDevicesRecView)
+        this.devicesDescText = this.findViewById(R.id.devicesDescText)
+        this.pairButton = this.findViewById(R.id.pairButton)
+        this.acceptButton = this.findViewById(R.id.acceptButton)
+        this.progressBar = this.findViewById(R.id.progressBar)
     }
 }
