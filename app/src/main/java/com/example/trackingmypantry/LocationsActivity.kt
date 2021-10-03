bd4fe164ed7cfc66a73b195b8cf60821510d1f9d
bogo8liuk <lucaborghi99@gmail.com>
2021-10-03 19:35:18 +0200

    LocationsActivity first infrastructure

--- app/src/main/java/com/example/trackingmypantry/LocationsActivity.kt
+++ app/src/main/java/com/example/trackingmypantry/LocationsActivity.kt
@@ -2,10 +2,27 @@ package com.example.trackingmypantry
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class LocationsActivity : AppCompatActivity() {
+class LocationsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_locations)
+        this.setContentView(R.layout.activity_locations)
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+        //TODO: get the API key
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        map.addMarker(
+            MarkerOptions()
+                .position(LatLng(0.0, 0.0))
+                .title("marker")
+        )
     }
 }
