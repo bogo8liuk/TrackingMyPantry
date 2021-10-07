18eccd2402cb9356945c38efa7c4339a10f29d42
bogo8liuk <lucaborghi99@gmail.com>
2021-10-07 10:24:17 +0200

    locations to remove row

--- app/src/main/java/com/example/trackingmypantry/RemoveLocationsActivity.kt
+++ app/src/main/java/com/example/trackingmypantry/RemoveLocationsActivity.kt
@@ -2,8 +2,15 @@ package com.example.trackingmypantry
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.widget.AppCompatButton
+import androidx.recyclerview.widget.RecyclerView
 
 class RemoveLocationsActivity : AppCompatActivity() {
+    private lateinit var descriptionText: TextView
+    private lateinit var recView: RecyclerView
+    private lateinit var removeButton: AppCompatButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_remove_locations)
