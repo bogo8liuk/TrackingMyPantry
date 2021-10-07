68b03f9037bd57753362880b8586ed42757b3e36
bogo8liuk <lucaborghi99@gmail.com>
2021-10-07 10:35:56 +0200

    locations to remove viewmodel

---
+++ app/src/main/java/com/example/trackingmypantry/lib/viewmodels/LocationsToRemoveViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.trackingmypantry.lib.viewmodels
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import com.example.trackingmypantry.db.entities.Place
+import com.example.trackingmypantry.lib.DbSingleton
+
+class LocationsToRemoveViewModel(app: Application): AndroidViewModel(app) {
+    private val appContext = app.applicationContext
+
+    private val locations = DbSingleton.getInstance(appContext).getAllPlaces()
+
+    fun getPlaces(): LiveData<List<Place>> {
+        return locations
+    }
+}
