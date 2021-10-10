26f664661865adcf5d971ba93428240a54a50f6e
bogo8liuk <lucaborghi99@gmail.com>
2021-10-10 16:36:47 +0200

    suggestions recycler view

--- app/src/main/java/com/example/trackingmypantry/SuggestionsActivity.kt
+++ app/src/main/java/com/example/trackingmypantry/SuggestionsActivity.kt
@@ -2,6 +2,8 @@ package com.example.trackingmypantry
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 
 /**
  * @warning To start this activity, it is mandatory to put the extra `ITEMS_ELSE_PLACES_EXTRA`
@@ -14,6 +16,13 @@ class SuggestionsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_suggestions)
+        this.setContentView(R.layout.activity_suggestions)
+
+        val recyclerView = this.findViewById<RecyclerView>(R.id.suggestionsRecView)
+        val noElemsText = this.findViewById<TextView>(R.id.noElemsAdviseText)
+
+        val itemsOrPlacesExtra = this.intent.extras!!.getBoolean(ITEMS_ELSE_PLACES_EXTRA)
+
+        //TODO
     }
 }
