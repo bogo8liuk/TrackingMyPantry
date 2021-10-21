27fae151fc46c79eb042472364ee475d0fbf8882
bogo8liuk <lucaborghi99@gmail.com>
2021-10-21 11:08:46 +0200

    onBindViewHolder

--- app/src/main/java/com/example/trackingmypantry/lib/adapters/ReceivedSuggestionsAdapter.kt
+++ app/src/main/java/com/example/trackingmypantry/lib/adapters/ReceivedSuggestionsAdapter.kt
@@ -3,6 +3,7 @@ package com.example.trackingmypantry.lib.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.AppCompatButton
 import androidx.recyclerview.widget.RecyclerView
 import com.example.trackingmypantry.R
 import com.example.trackingmypantry.lib.data.Suggestion
@@ -13,7 +14,13 @@ class ReceivedSuggestionsAdapter(
 ): RecyclerView.Adapter<ReceivedSuggestionsAdapter.ViewHolder>() {
 
     inner class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        val suggestionButton = view.findViewById<AppCompatButton>(R.id.receivedSuggestionButton)
 
+        init {
+            suggestionButton.setOnClickListener {
+                suggestionCallback(IndexedArray(suggestions, this.adapterPosition))
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -27,7 +34,19 @@ class ReceivedSuggestionsAdapter(
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        if (suggestions[position].isItem()) {
+            holder.suggestionButton.text = suggestions[position].itemSuggestion!!.name
+
+            holder.suggestionButton.setCompoundDrawablesWithIntrinsicBounds(
+                R.drawable.ic_baseline_location_on_dark_24, 0, 0, 0
+            )
+        } else if (suggestions[position].isPlace()) {
+            holder.suggestionButton.text = suggestions[position].placeSuggestion!!.title
+
+            holder.suggestionButton.setCompoundDrawablesWithIntrinsicBounds(
+                R.drawable.ic_baseline_food_bank_24, 0, 0, 0
+            )
+        }
     }
 
     override fun getItemCount(): Int {
