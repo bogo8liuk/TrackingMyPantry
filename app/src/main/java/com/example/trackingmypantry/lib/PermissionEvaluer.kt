26eff86df33f13a1d8265619dac1b874dbe6dca0
bogo8liuk <lucaborghi99@gmail.com>
2021-11-07 14:33:28 +0100

    PermissionEvaluer updated

--- app/src/main/java/com/example/trackingmypantry/lib/PermissionEvaluer.kt
+++ app/src/main/java/com/example/trackingmypantry/lib/PermissionEvaluer.kt
@@ -3,6 +3,7 @@ package com.example.trackingmypantry.lib
 import android.app.Activity
 import android.content.Context
 import android.content.pm.PackageManager
+import android.os.Build
 import androidx.core.app.ActivityCompat
 
 class PermissionEvaluer {
@@ -14,7 +15,23 @@ class PermissionEvaluer {
         }
 
         fun request(activity: Activity, permission: String, requestCode: Int = DEFAULT_REQUEST_CODE) {
-            ActivityCompat.requestPermissions(activity, arrayOf(permission), requestCode)
+            /*
+            * If app is running Android 12 or higher, then ACCESS_FINE_LOCATION must be requested
+            * along with ACCESS_COARSE_LOCATION in a single request.
+            */
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S &&
+                permission == android.Manifest.permission.ACCESS_FINE_LOCATION) {
+                ActivityCompat.requestPermissions(
+                    activity,
+                    arrayOf(
+                        android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                        permission
+                    ),
+                    requestCode
+                )
+            } else {
+                ActivityCompat.requestPermissions(activity, arrayOf(permission), requestCode)
+            }
         }
     }
 }
